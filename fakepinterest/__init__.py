1f5190c54c4fea7dd7c094de09b5e86f5c64b9a8
Isabela Mattos <magalhaes569@gmail.com>
2025-02-19 21:16:26 -0300

    Organização do projeto alterada, banco de dados criado, estruturas de login e senha também criados, rotas atualizadas

---
+++ fakepinterest/__init__.py
@@ -0,0 +1,14 @@
+# criar aplicativo, banco de dados, estrutura geral
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SECRET_KEY"] = "e4a8bb04-0083-4472-bf7a-405a0b361b38c9a695dcb69344bc678144d458a3789a"
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'homepage'
