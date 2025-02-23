593f50c33dbe11ce60a7902bb28e600743ac614b
Isabela Magalhães <magalhaes569@gmail.com>
2025-02-23 17:10:43 -0300

    Implementação das funcionalidades de criar conta e login

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "e4a8bb04-0083-4472-bf7a-405a0b361b38c9a695dcb69344bc678144d458a3789a"
@@ -12,3 +13,5 @@ database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'homepage'
+
+from fakepinterest import routes
--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -1,1 +1,23 @@
 # criar os formulários do projeto
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fakepinterest.models import Usuario
+
+class FormLogin(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    botao_confirmacao = SubmitField('Fazer Login')
+
+
+class FormCriarConta(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(min=6, max=10)])
+    confirmacao_senha = PasswordField('Confirmação de Senha', validators=[DataRequired(), EqualTo('senha')])
+    botao_confirmacao = SubmitField('Criar Conta')
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            return ValidationError('E-mail já cadastrado. Tente outro.')
