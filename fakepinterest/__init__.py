680d00fb135a8d13b8fa175060b51fbf9a5b310d
Isabela Magalhães <magalhaes569@gmail.com>
2025-02-24 21:56:58 -0300

    Atualizações nas funcionalidades de 'perfil de usuário' e 'uploads de fotos'

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -8,6 +8,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "e4a8bb04-0083-4472-bf7a-405a0b361b38c9a695dcb69344bc678144d458a3789a"
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
